42fd53ffe67ad95250689b98e79ec3c894458825
p-h-c-s <pedroceravolo@jusbrasil.com.br>
2024-07-26 15:01:53 -0300

    finished

--- src/main.rs
+++ src/main.rs
@@ -23,15 +23,8 @@ struct StationData {
     temp_sum: i32,
 }
 
-// To use the `{}` marker, the trait `fmt::Display` must be implemented
-// manually for the type.
 impl fmt::Display for StationData {
-    // This trait requires `fmt` with this exact signature.
     fn fmt(&self, f: &mut fmt::Formatter) -> fmt::Result {
-        // Write strictly the first element into the supplied output
-        // stream: `f`. Returns `fmt::Result` which indicates whether the
-        // operation succeeded or failed. Note that `write!` uses syntax which
-        // is very similar to `println!`.
         let mean = (self.temp_sum as f64 / TEMP_DIVISOR) / self.count as f64;
         write!(f, "{:.1}/{:.1}/{:.1}", (self.min_temp as f64 / TEMP_DIVISOR), mean, (self.max_temp as f64 / TEMP_DIVISOR))
     }
@@ -182,10 +175,13 @@ fn process_mmap<'scope, 'env>(mmap: &'env [u8], chunk_size: usize, s: &'scope Sc
         v.sort_by_key(|e| e.0);
         v
     };
-    for (k, v) in vec {
+    for (i, (k, v)) in vec.iter().enumerate() {
+        if i == vec.len() - 1 {
+            write!(stdout, "{}={}}}", k, v).unwrap();
+            continue;
+        }
         write!(stdout, "{}={}, ", k, v).unwrap();
     }
-    stdout.write(b"}").unwrap();
 }
 
 fn main() -> io::Result<()> {
