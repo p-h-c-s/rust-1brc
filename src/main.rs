ee6801fa877c2125cc0eec9f1a9de2f9c4ed8650
p-h-c-s <pedroceravolo@jusbrasil.com.br>
2024-07-26 00:58:18 -0300

    avoid vtable

--- src/main.rs
+++ src/main.rs
@@ -7,7 +7,7 @@ use std::rc::Rc;
 use std::str::{from_utf8, from_utf8_unchecked};
 use std::sync::mpsc::{Receiver, Sender};
 use std::sync::{mpsc, Arc, Mutex};
-use std::thread::{self, ScopedJoinHandle};
+use std::thread::{self, Scope, ScopedJoinHandle};
 
 pub mod mmap;
 
@@ -47,7 +47,7 @@ impl StationData {
     fn parse_temp<'a>(temp: &'a str) -> i32 {
         let mut result: i32 = 0;
         let mut negative: bool = false;
-        for (i, ch) in temp.chars().enumerate() {
+        for ch in temp.chars() {
             match ch {
                 '0'..='9' => {
                     result = result * 10 + (ch as i32 - '0' as i32);
@@ -65,7 +65,6 @@ impl StationData {
         result
     }
 
-    // slow!
     fn parse_data<'a>(raw: &'a str) -> (&'a str, i32) {
         let (name, temp) = raw.split_once(";").unwrap();
         (name, Self::parse_temp(temp))
@@ -110,7 +109,53 @@ const MAX_STATION_NAME_SIZE: usize = 100;
 // 5 bytes for two digit float number with a single fractional digit and `;` character
 // idea to divide file: pad each line up to MAX_LINE_SIZE bytes
 const MAX_LINE_SIZE: usize = MAX_STATION_NAME_SIZE + 5;
-const NUM_CONSUMERS: usize = 16;
+const NUM_CONSUMERS: usize = 8;
+
+fn process_chunk<'a>(current_chunk_slice: &'a [u8]) -> BTreeMap<&'a str, StationData>{
+    let mut station_map: BTreeMap<&str, StationData> = BTreeMap::new();
+    let lines = unsafe{from_utf8_unchecked(current_chunk_slice)};
+    for line in lines.lines() {
+        let (name, temp) = StationData::parse_data(&line);
+        match station_map.get_mut(name) {
+            Some(station) => station.update_from(temp),
+            None => {
+                station_map.insert(name, StationData::new(temp));
+            }
+        };
+    }
+    return station_map;
+}
+
+fn process_mmap<'scope, 'env>(mmap: &'env [u8], chunk_size: usize, s: &'scope Scope<'scope, 'env>) {
+    let mut handlers: Vec<ScopedJoinHandle<BTreeMap<&str, StationData>>> = Vec::new();
+    // let file_string_slice = unsafe {from_utf8_unchecked(mmap)};
+    let mut last_chunk_offset: usize = 0;
+    for chunk_num in 0..NUM_CONSUMERS {
+        let new_line_data = get_nearest_newline(mmap, chunk_num, chunk_size, last_chunk_offset);
+        let current_chunk_slice = new_line_data.0;
+        last_chunk_offset = new_line_data.1;
+
+        let h = s.spawn(move || process_chunk(current_chunk_slice));
+        handlers.push(h);
+    }
+    let mut station_map: BTreeMap<&str, StationData> = BTreeMap::new();
+    for h in handlers {
+        let inner_station = h.join().unwrap();
+        station_map = merge_btrees( station_map, inner_station);
+    }
+    // write to stdio
+    let mut stdout = io::stdout().lock();
+    stdout.write(b"{").unwrap();
+    for (k, v) in station_map.into_iter() {
+        // ("{}={}/{}/{}", k, v.min_temp, v.mean_temp, v.max_temp)
+        write!(
+            stdout,
+            "{}={}/{}/{}, ",
+            k, v.min_temp, v.count, v.max_temp
+        ).unwrap();
+    }
+    stdout.write(b"}").unwrap();
+}
 
 fn main() -> io::Result<()> {
     // won't accept non-utf-8 args
@@ -128,52 +173,9 @@ fn main() -> io::Result<()> {
 
     let chunk_size = f_size as usize / NUM_CONSUMERS;
 
-    let station_map = thread::scope(|s|{
-        let mut handlers: Vec<ScopedJoinHandle<BTreeMap<&str, StationData>>> = Vec::new();
-        // let file_string_slice = unsafe {from_utf8_unchecked(mmap)};
-        let mut last_chunk_offset: usize = 0;
-        for chunk_num in 0..NUM_CONSUMERS {
-            let new_line_data = get_nearest_newline(mmap, chunk_num, chunk_size, last_chunk_offset);
-            let current_chunk_slice = new_line_data.0;
-            last_chunk_offset = new_line_data.1;
+    thread::scope(|s| process_mmap(mmap, chunk_size, s));
 
-            let h = s.spawn(move || {
-                let mut station_map: BTreeMap<&str, StationData> = BTreeMap::new();
-                let lines = unsafe{from_utf8_unchecked(current_chunk_slice)};
-                for line in lines.lines() {
-                    let (name, temp) = StationData::parse_data(&line);
-                    match station_map.get_mut(name) {
-                        Some(station) => station.update_from(temp),
-                        None => {
-                            station_map.insert(name, StationData::new(temp));
-                        }
-                    };
-                }
-                return station_map;
-            });
-            handlers.push(h);
-        }
-        let station_map: BTreeMap<&str, StationData> = BTreeMap::new();
-        handlers.into_iter().fold(station_map, |s1, s2| {
-            let inner_station = s2.join().unwrap();
-            merge_btrees(s1, inner_station)
-        })
-    });
 
-    {
-        // write to stdio
-        let mut stdout = io::stdout().lock();
-        stdout.write(b"{").unwrap();
-        for (k, v) in station_map.into_iter() {
-            // ("{}={}/{}/{}", k, v.min_temp, v.mean_temp, v.max_temp)
-            write!(
-                stdout,
-                "{}={}/{}/{}, ",
-                k, v.min_temp, v.count, v.max_temp
-            ).unwrap();
-        }
-        stdout.write(b"}").unwrap();
-    }
 
 
     Ok(())
